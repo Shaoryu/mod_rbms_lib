f5b18189d0303895cc229091bb421e61cf66bb13
Shaoryu <149553583+Shaoryu@users.noreply.github.com>
2025-07-11 18:28:10 +0900

    Update rbms.h

--- rbms.h
+++ rbms.h
@@ -15,7 +15,7 @@ class rbms {
         void spd_control(int* set_speed,int* motor);
         
     
-
+    private:
         CANMessage _canMessage,_canMessage2,_msg;
         CAN &_can;
         bool _motor_type;//if 0 m2006,if 1 m3508
