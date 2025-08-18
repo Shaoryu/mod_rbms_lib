b6fbfbbac1116db2795665b722a4ce80c56cf166
Shaoryu <149553583+Shaoryu@users.noreply.github.com>
2025-08-18 16:09:07 +0900

    Update rbms.cpp

--- rbms.cpp
+++ rbms.cpp
@@ -1,43 +1,47 @@
 #include "rbms.h"
 #include "mbed.h"
-#include <cmath>
-#include<stdlib.h>
-
-rbms::rbms(CAN &can,bool motor_type,int motor_num)
-    : _can(can),_motor_type(motor_type),_motor_num(motor_num){
-    if(_motor_type){
-        _motor_max=16384;//m3508
-    }else{
-        _motor_max=10000;//m2006
-    }
-    if(_motor_num<=8){
-        _can.frequency(1000000); // CANのビットレートを指定
-        _can.mode(CAN::Normal); // CANのモードをNormalに設定
-    }
-}
+#include <cstdlib>
 
 rbms::rbms(CAN &can,bool* motor_type,int motor_num)
-    : _can(can),_motor_type_pointer(motor_type),_motor_num(motor_num){
-    _motor_max_po = (int*)malloc(_motor_num*sizeof(int));
+    : _can(can),_motor_type(motor_type),_motor_num(motor_num){
+    _motor_max=(int*)malloc(sizeof(int)*_motor_num);
     for(int i=0;i<_motor_num;i++){
-        if(_motor_type_pointer[i]){
-            _motor_max_po[i]=16384;//m3508
+        if(_motor_type[i]){
+            _motor_max[i]=16384;//m3508
         }else{
-            _motor_max_po[i]=10000;//m2006
+            _motor_max[i]=10000;//m2006
         }
-        //printf("%d\r\n",_motor_max_po[i]);
     }
     if(_motor_num<=8){
         _can.frequency(1000000); // CANのビットレートを指定
         _can.mode(CAN::Normal); // CANのモードをNormalに設定
     }
 }
+
+rbms::rbms(CAN &can,bool motor_type,int motor_num): _can(can),_motor_num(motor_num){//未確認
+    _motor_max=(int*)malloc(sizeof(int)*_motor_num);
+    _motor_type=(bool*)malloc(sizeof(bool)*_motor_num);
+    for(int i=0;i<_motor_num;i++){
+        _motor_type[i]= motor_type;
+        if(_motor_type[i]){
+            _motor_max[i]=16384;//m3508
+        }else{
+            _motor_max[i]=10000;//m2006
+        }
+    }
+    if(_motor_num<=8){
+        _can.frequency(1000000); // CANのビットレートを指定
+        _can.mode(CAN::Normal); // CANのモードをNormalに設定
+    }
+}
+
+
 
 int rbms::rbms_send(int* motor) {//motorへ制御信号を送信する関数
     char _byte[_motor_num*2];//byteデータ変換用
     int _a=0;
     for(int i=0;i<_motor_num;i++){  //int dataを2byteに分割
-        if(motor[i]>_motor_max_po[i])return 0;//入力値がmotor上限以上の場合return0
+        if(motor[i]>_motor_max[i])return 0;//入力値がmotor上限以上の場合return0
         _byte[_a++] = (char)(motor[i] >> 8); // int値の上位8ビットをcharに変換
         _byte[_a++] = (char)(motor[i] & 0xFF); // int値の下位8ビットをcharに変換
     }
@@ -81,7 +85,7 @@ void rbms::rbms_read(CANMessage &msg, short *rotation,short *speed) {//motorか�
             _r = (msg.data[0] << 8) | (msg.data[1] & 0xff);//2byteに分割されているdataを結合
             _rotation = (float)_r / 8192 * 360;//8192=360°
             *rotation=_rotation;
- 
+
             _speed = (msg.data[2] << 8) | (msg.data[3] & 0xff);
             if (_speed & 0b1000000000000000){//マイナス値の場合(最上位ビットが1のとき)(2の補数)
                 _speed--;
@@ -96,7 +100,7 @@ void rbms::rbms_read(CANMessage &msg, short *rotation,short *speed) {//motorか�
             }
 
             _temperature = msg.data[6];
-            
+
 }
 
 void rbms::can_read(){//motorからdata受信
@@ -127,19 +131,19 @@ void rbms::spd_control(int* set_speed,int* motor){//速度制御用関数
         ie[i]=0.0;
         tm[i].start();
     }
-    
+
     while(1){
         for(int id=0;id<_motor_num;id++){
             if(_msg.id==0x201+id){//esc idごとに受信データ割り振り
                 CANMessage msg=_msg;
                 rbms_read(msg,&rotation[id],&speed[id]);//data変換
-                if(_motor_type){
+                if(_motor_type[id]){
                     motor[id] = (int)pid(tm[id].read(),speed[id]/19,set_speed[id],&delta_rpm_pre[id],&ie[id]);
                 }else{
                     motor[id] = (int)pid(tm[id].read(),speed[id]/36,set_speed[id],&delta_rpm_pre[id],&ie[id],15,6);
                 }
                 tm[id].reset();//timer reset
-                if(motor[id]>_motor_max_po[id]){motor[id]=_motor_max_po[id];}else if(motor[id]<-_motor_max_po[id]){motor[id]=-_motor_max_po[id];}//上限確認超えてた場合は上限値にset
+                if(motor[id]>_motor_max[id]){motor[id]=_motor_max[id];}else if(motor[id]<-_motor_max[id]){motor[id]=-_motor_max[id];}//上限確認超えてた場合は上限値にset
             }
         }
         ThisThread::sleep_for(3ms);
